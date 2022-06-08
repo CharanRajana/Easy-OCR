4596964922cfea366e8f4e5ed775deeed4f3807f
Charan Rajana <saicharanrajana@gmail.com>
2022-06-08 22:23:50 +0530

    Added screenshots and updated history screen

--- lib/pages/history.dart
+++ lib/pages/history.dart
@@ -1,19 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:quick_scan/extras/fonts.dart';
 
 class History extends StatelessWidget {
   const History({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.indigo.shade900,
-      child: const Center(
-        child: Text(
-          'Screen three',
-          style: TextStyle(
-            fontSize: 36,
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.amber,
+        child: const Icon(Icons.clear_all),
+      ),
+      backgroundColor: Colors.indigo.shade900,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 30,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Text(
+                  'HISTORY',
+                  style: bigText,
+                ),
+              ),
+            ],
           ),
         ),
       ),
